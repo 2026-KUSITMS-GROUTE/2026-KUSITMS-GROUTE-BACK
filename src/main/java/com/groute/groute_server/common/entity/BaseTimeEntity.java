77edc98eb42fcbb15f3dbbb5c82477357e77f627
박도현 <parkdo0918@gmail.com>
2026-04-18 00:46:11 +0900

    chore: BaseTimeEntity 및 SoftDeleteEntity 공통 엔티티 클래스 생성

---
+++ src/main/java/com/groute/groute_server/common/entity/BaseTimeEntity.java
@@ -0,0 +1,28 @@
+package com.groute.groute_server.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+/**
+ * 생성 시각 및 수정 시각 자동 관리 엔티티 부모 클래스
+ */
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/groute/groute_server/common/entity/SoftDeleteEntity.java
@@ -0,0 +1,21 @@
+package com.groute.groute_server.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+/**
+ * 논리 삭제(Soft Delete) 지원 엔티티 부모 클래스
+ */
+@Getter
+@MappedSuperclass
+public abstract class SoftDeleteEntity extends BaseTimeEntity {
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
