c3615887ca3188c29dc8a6fb9e87d516513e3253
Gyeore Kim <h2weirdo@icloud.com>
2026-04-18 19:29:46 +0900

    chore: SecurityConfig에 ErrorCode 문서 경로 permitAll 추가

--- src/main/java/com/groute/groute_server/common/config/SecurityConfig.java
+++ src/main/java/com/groute/groute_server/common/config/SecurityConfig.java
@@ -18,7 +18,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/docs/error-code"
                         ).permitAll()
                         .anyRequest().authenticated()
                 );
