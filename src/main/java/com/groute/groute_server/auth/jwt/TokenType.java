40c87a82a7b91c4090b73d7470bc3317bbe76e18
Gyeore Kim <h2weirdo@icloud.com>
2026-04-22 20:34:02 +0900

    feat: JwtTokenProvider 및 JwtProperties 구현

---
+++ src/main/java/com/groute/groute_server/auth/jwt/TokenType.java
@@ -0,0 +1,11 @@
+package com.groute.groute_server.auth.jwt;
+
+/**
+ * JWT 토큰 종류.
+ *
+ * <p>재발급(ONB001) 시 refresh 토큰만 허용하고, 인증 필터에서는 access 토큰만 허용하도록 구분하기 위해 claim으로 저장한다.
+ */
+public enum TokenType {
+    ACCESS,
+    REFRESH
+}
