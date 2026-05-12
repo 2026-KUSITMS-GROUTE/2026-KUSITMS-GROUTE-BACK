66c2ff129fe62151d5359aa32394cfabba8c571d
hyhy-j <lhjlhj10088@gmail.com>
2026-05-12 21:43:17 +0900

    test: categories 전체 0 검증 단언 강화

--- src/test/java/com/groute/groute_server/GrouteServerApplicationTests.java
+++ src/test/java/com/groute/groute_server/GrouteServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.groute.groute_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class GrouteServerApplicationTests {
 
     @Test
