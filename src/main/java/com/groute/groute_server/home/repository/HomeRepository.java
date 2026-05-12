3503f928128841b4082aa206ee4792c26f482cb2
hyhy-j <lhjlhj10088@gmail.com>
2026-05-12 20:42:11 +0900

    feat: 홈 화면 역량 레이더 STAR 집계 리포지토리 추가

---
+++ src/main/java/com/groute/groute_server/home/repository/HomeRepository.java
@@ -0,0 +1,25 @@
+package com.groute.groute_server.home.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.groute.groute_server.record.domain.StarRecord;
+import com.groute.groute_server.record.domain.enums.StarRecordStatus;
+
+public interface HomeRepository extends JpaRepository<StarRecord, Long> {
+
+    /** TAGGED 완료된 STAR 기록의 역량 카테고리별 건수. selectedCompetency가 NULL인 스크럼은 제외. */
+    @Query(
+            "SELECT s.selectedCompetency, COUNT(sr) "
+                    + "FROM StarRecord sr JOIN sr.scrum s "
+                    + "WHERE sr.user.id = :userId "
+                    + "AND sr.status = :tagged "
+                    + "AND sr.isDeleted = false "
+                    + "AND s.selectedCompetency IS NOT NULL "
+                    + "GROUP BY s.selectedCompetency")
+    List<Object[]> countCompletedByCompetency(
+            @Param("userId") Long userId, @Param("tagged") StarRecordStatus tagged);
+}
