f1a7836cc98d07064c6997c28ee4651bf5b816f1
hyhy-j <lhjlhj10088@gmail.com>
2026-05-17 00:17:50 +0900

    refactor: Logger 선언을 @Slf4j 애노테이션으로 교체

--- src/main/java/com/groute/groute_server/common/transaction/AfterCommitExecutor.java
+++ src/main/java/com/groute/groute_server/common/transaction/AfterCommitExecutor.java
@@ -1,7 +1,7 @@
 package com.groute.groute_server.common.transaction;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.support.TransactionSynchronization;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
@@ -12,10 +12,9 @@ import org.springframework.transaction.support.TransactionSynchronizationManager
  * <p>활성 트랜잭션이 있으면 커밋 후 실행하고, 없으면(테스트 등) 즉시 실행한다. S3 삭제처럼 롤백 불가능한 외부 I/O를 트랜잭션 바깥으로 분리할 때 사용한다.
  * afterCommit 콜백에서 발생한 예외는 트랜잭션이 이미 커밋된 상태이므로 클라이언트에 전파하지 않고 로깅만 한다.
  */
+@Slf4j
 @Component
 public class AfterCommitExecutor {
-
-    private static final Logger log = LoggerFactory.getLogger(AfterCommitExecutor.class);
 
     public void execute(Runnable task) {
         if (TransactionSynchronizationManager.isSynchronizationActive()) {
